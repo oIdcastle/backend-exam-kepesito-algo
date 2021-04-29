96f5bd2d36ef3e4470e7d03d3fea76fd5d87e00f
oldcastle <32189785+oIdcastle@users.noreply.github.com>
2021-04-29 10:17:55 +0200

    Implement addDigits() method

--- src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
+++ src/main/java/hu/nive/ujratervezes/adddigits/AddDigits.java
@@ -1,5 +1,15 @@
 package hu.nive.ujratervezes.adddigits;
 
 public class AddDigits {
-    //todo: implement your solution here
+
+    int addDigits(String input) {
+        if (input == null || input == "") {
+            return -1;
+        }
+        String digits = input.replaceAll("[^0-9]", "");
+        return digits.chars()
+                .map(c -> Character.getNumericValue(c))
+                .sum();
+    }
+
 }
